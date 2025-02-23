b5ac55eb37df4f0d35d99ba1a58f5b616f67f015
nguyenvu-laptop <nguye@LAPTOP>
2025-02-24 01:25:04 +0700

    ch03-006 Adding Config Class for Clients, Resources, Scopes and TestUsers_en

--- src/IdentityServer/Program.cs
+++ src/IdentityServer/Program.cs
@@ -1,6 +1,13 @@
+using IdentityServer;
+
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddIdentityServer();
+builder.Services.AddIdentityServer()
+                .AddInMemoryClients(Config.Clients)
+                .AddInMemoryApiScopes(Config.ApiScopes)
+                .AddInMemoryIdentityResources(Config.IdentityResources)
+                .AddTestUsers(Config.TestUsers)
+                .AddDeveloperSigningCredential();
 
 var app = builder.Build();
 
