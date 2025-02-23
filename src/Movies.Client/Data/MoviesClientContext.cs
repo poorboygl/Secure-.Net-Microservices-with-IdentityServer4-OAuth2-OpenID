296ef4c1e10d6efbc95a3dc1ba38c971759f5c74
nguyenvu-laptop <nguye@LAPTOP>
2025-02-24 03:23:33 +0700

    ch05-004 Create Controller Class for Movie Model

---
+++ src/Movies.Client/Data/MoviesClientContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using BuildingBlock.Models;
+
+namespace Movies.Client.Data
+{
+    public class MoviesClientContext : DbContext
+    {
+        public MoviesClientContext (DbContextOptions<MoviesClientContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movie { get; set; }
+    }
+}
