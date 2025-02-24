6773dfa0b3d7d4b7b31574edd8a5837528c659f6
nguyenvu-laptop <nguye@LAPTOP>
2025-02-24 09:20:35 +0700

    ch07-002 Build OpenId Connect Interactive Client for Movies.Client MVC Application

--- src/Movies.Client/Startup.cs
+++ src/Movies.Client/Startup.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -22,6 +24,44 @@ namespace Movies.Client
             services.AddControllersWithViews();
 
             services.AddScoped<IMovieApiService, MovieApiService>();
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+            })
+                .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme)
+                .AddOpenIdConnect(OpenIdConnectDefaults.AuthenticationScheme, options =>
+                {
+                    options.Authority = "https://localhost:5005";
+
+                    options.ClientId = "movies_mvc_client";
+                    options.ClientSecret = "secret";
+                    options.ResponseType = "code";
+
+                    options.Scope.Add("openid");
+                    options.Scope.Add("profile");
+                    //options.Scope.Add("address");
+                    //options.Scope.Add("email");
+                    //options.Scope.Add("roles");
+
+                    //options.ClaimActions.DeleteClaim("sid");
+                    //options.ClaimActions.DeleteClaim("idp");
+                    //options.ClaimActions.DeleteClaim("s_hash");
+                    //options.ClaimActions.DeleteClaim("auth_time");
+                    //options.ClaimActions.MapUniqueJsonKey("role", "role");
+
+                    //options.Scope.Add("movieAPI");
+
+                    options.SaveTokens = true;
+                    options.GetClaimsFromUserInfoEndpoint = true;
+
+                    //options.TokenValidationParameters = new TokenValidationParameters
+                    //{
+                    //    NameClaimType = JwtClaimTypes.GivenName,
+                    //    RoleClaimType = JwtClaimTypes.Role
+                    //};
+                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -42,6 +82,7 @@ namespace Movies.Client
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
